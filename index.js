7d5409d92b8b0b501a6d2fc2f3e7b92d7180dcb3
BuildFlow <saoud@zadunited.com>
2026-05-21 15:38:49 +0300

    add static files

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static('.'));
 // Supabase client
 const supabase = createClient(
   process.env.SUPABASE_URL,
